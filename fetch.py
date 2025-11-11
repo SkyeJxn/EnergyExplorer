bd99fffe16cc448e0281a4becc16224ae42d8bc5
Skye Jonczik <jeanjonczik2003@gmail.com>
2025-11-11 12:06:38 +0100

    fix: fix fetch script (production api)

--- fetch.py
+++ fetch.py
@@ -1,59 +1,47 @@
 import requests as rq
 import time as tm
 import sqlite3 as sql
-import json
-
-API_types = {"Hydro": ['Hydro pumped storage consumption', 'Hydro Run-of-River', 'Hydro water reservoir', 'Hydro pumped storage'],
-             "Biomass":['Biomass'],
-             "coal" : ['Fossil brown coal / lignite','Fossil hard coal'],
-             "Oil and Gas": ['Fossil oil', 'Fossil coal-derived gas', 'Fossil gas'],
-             "Wind": ['Wind offshore', 'Wind onshore'],
-             "Solar": ['Solar'],
-             "Others" : ['Geothermal', 'Others','Waste'],
-             "ren_share":['Renewable share of generation'],
-             "unwanted": ['Cross border electricity trading','Load (incl. self-consumption)', 'Residual load', 'Renewable share of load']
-             }
-
-last_found = "1762124400"
-
-now = int(tm.time())
-
-link = f"https://api.energy-charts.info/total_power?country=de&start={last_found}&end={last_found}"
-#link = f"https://api.energy-charts.info/total_power?country=de&start={last_found}&end={now}"
-
-def filtering(d):
-    res = [entry for entry in d if entry["name"] not in API_types["unwanted"]]
-    return res
-
-def addition(d):
-    res = 0
-    for entry in d:
-        if entry["name"] in API_types["Hydro"]:
-            res["Hydro"] += entry["values"][-1][1]
-        if entry["name"] in API_types["coal"]:
-            res["Coal"] += entry["values"][-1][1]
-        if entry["name"] in API_types["Oil and Gas"]:
-            res["Oil and Gas"] += entry["values"][-1][1]
-        if entry["name"] == API_types["Wind"]:
-            res["Wind"] += entry["values"][-1][1]
-        if entry["name"] == API_types["Others"]:
-            res["Others"] += entry["values"][-1][1]
-        if entry["name"] in API_types["Biomass"]:
-            res["Biomass"] += entry["values"][-1][1]
-        if entry["name"] in API_types["Solar"]:
-            res["Solar"] += entry["values"][-1][1]
-        if entry["name"] in API_types["ren_share"]:
-            res["ren_share"] = entry["values"][-1][1]    
-    return res
+import pandas as pd
 
 def data_fetch(url):
     response = rq.get(url)
-    return response
+    return response.json()
 
-df = data_fetch(link)
-filtered = filtering(df.json())
-aggregated = addition(filtered)
-print("Data: ", aggregated)
+def build_production_df(data, key_name, value_name, outer_key, index_name):
+    flat_dict = {item[key_name]: item[value_name] for item in data[outer_key]}
+    df = pd.DataFrame(flat_dict, index=data[index_name])
+    return df
 
-print("HTTP: ", df.status_code)
+sqlite_file = 'data.db'
 
+sql_connection = sql.connect(sqlite_file)
+sql_cursor = sql_connection.cursor()
+
+prod_sums = {"Hydro": ['Hydro Run-of-River', 'Hydro water reservoir', 'Hydro pumped storage'],
+             "Coal" : ['Fossil brown coal / lignite','Fossil hard coal'],
+             "Oil_Gas": ['Fossil oil', 'Fossil coal-derived gas', 'Fossil gas'],
+             "Wind": ['Wind offshore', 'Wind onshore'],
+             "Others" : ['Geothermal', 'Others','Waste'],
+             "Ren_share":['Renewable share of generation'],
+             }
+
+unwanted_prod = ['Hydro pumped storage consumption', 'Cross border electricity trading','Load (incl. self-consumption)', 'Residual load', 'Renewable share of load']
+
+last_found = sql_cursor.execute("SELECT MAX(Timestamp) FROM production").fetchone()[0]
+
+now = int(tm.time())
+
+link = f"https://api.energy-charts.info/total_power?country=de&start={last_found}&end={now}"
+
+resp = data_fetch(link)
+
+df = build_production_df(resp, 'name', 'data', 'production_types', 'unix_seconds').drop(columns=unwanted_prod)
+
+df["Total_Production"] = df.sum(axis=1)
+
+for key, values in prod_sums.items():
+    df[key] = df[values].sum(axis=1)
+
+df = df.drop(columns=[item for sublist in prod_sums.values() for item in sublist if item != 'Others'])
+
+pd.io.sql.to_sql(df, 'production', sql_connection, if_exists='append', index_label='Timestamp')
