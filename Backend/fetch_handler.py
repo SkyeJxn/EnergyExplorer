9f8b15a2db61e452816917b960270b43eaf810a6
Skye Jonczik <jeanjonczik2003@gmail.com>
2025-12-02 17:59:59 +0100

    fix: close connections after write

--- Backend/fetch_handler.py
+++ Backend/fetch_handler.py
@@ -2,15 +2,13 @@ from API_fetch import price_fetch, prod_fetch
 import sqlite3 as sql
 import os
 
-DB_Path = os.environ.get("DATABASE_PATH", "data/data.db")
-conn = sql.connect(DB_Path)
-cur = conn.cursor()
-
+DB_Path = os.environ.get("DATABASE_PATH", "../data/data.db")
 price_link = "https://api.energy-charts.info/price?bzn=DE-LU"
 prod_link = "https://api.energy-charts.info/total_power?country=de"
-
-price_fetch(price_link, conn, cur)
-prod_fetch(prod_link, conn, cur)
-
-conn.commit()
-conn.close()
+ 
+with sql.connect(DB_Path) as conn:
+    cur = conn.cursor()
+    price_fetch(price_link, conn, cur)
+    prod_fetch(prod_link, conn, cur)
+    conn.commit()
+    cur.close()
