bdb9bf49c3cebf53b367eff1081af44f234607b1
Skye Jonczik <jeanjonczik2003@gmail.com>
2025-12-04 19:48:30 +0100

    refactor: add api flags, remove debug print

--- Backend/fetch_handler.py
+++ Backend/fetch_handler.py
@@ -5,9 +5,9 @@ import argparse as arg
 
 parser = arg.ArgumentParser(description="API Fetch Script")
 parser.add_argument('-i', action='store_true', help="Initial API call without table lookup")
+parser.add_argument('-prod', action='store_true', help="call prod API")
+parser.add_argument('-price', action='store_true', help="call price API")
 flags = parser.parse_args()
-
-print(flags.i)
 
 DB_Path = os.environ.get("DATABASE_PATH", "./data/data.db")
 price_link = "https://api.energy-charts.info/price?bzn=DE-LU"
@@ -15,7 +15,9 @@ prod_link = "https://api.energy-charts.info/total_power?country=de"
  
 with sql.connect(DB_Path) as conn:
     cur = conn.cursor()
-    price_fetch(price_link, conn, cur, flags.i)
-    prod_fetch(prod_link, conn, cur, flags.i)
+    if (flags.price):
+        price_fetch(price_link, conn, cur, flags.i)
+    if (flags.prod):
+        prod_fetch(prod_link, conn, cur, flags.i)
     conn.commit()
     cur.close()
