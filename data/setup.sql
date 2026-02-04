b445d107444b4182423aa71c53974fadf8ff68ff
Skye Jonczik <jeanjonczik2003@gmail.com>
2026-02-04 18:02:21 +0100

    refactor: restructure database

--- data/setup.sql
+++ data/setup.sql
@@ -4,14 +4,22 @@ DROP TABLE IF EXISTS prices;
 CREATE TABLE production (
     Timestamp INTEGER PRIMARY KEY,
     Biomass REAL,
+    Biomass_pct REAL,
     Coal REAL,
+    Coal_pct REAL,
     Hydro REAL,
+    Hydro_pct REAL,
     Oil_Gas REAL,
+    Oil_Gas_pct REAL,
     Others REAL,
+    Others_pct REAL,
     Solar REAL,
-    Total_Production REAL,
+    Solar_pct REAL,
     Wind REAL,
-    Ren_share REAL
+    Wind_pct REAL,
+    Total_Production REAL,
+    Ren_share REAL,
+    Ren_share_bin REAL
 );
 
 CREATE TABLE prices (
