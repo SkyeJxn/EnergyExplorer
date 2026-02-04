8abc0fd13fc8b74e615cd34d7cabaed7694a2990
Skye Jonczik <jeanjonczik2003@gmail.com>
2026-02-04 18:02:45 +0100

    refactor: restructure API Fetching

--- Backend/API_fetch.py
+++ Backend/API_fetch.py
@@ -29,19 +29,34 @@ def prod_fetch(url, sql_connection, sql_cursor, init = False):
              "Oil_Gas": ['Fossil oil', 'Fossil coal-derived gas', 'Fossil gas'],
              "Wind": ['Wind offshore', 'Wind onshore'],
              "Others" : ['Geothermal', 'Others','Waste'],
-             "Ren_share":['Renewable share of generation'],
              }
-    unwanted_prod = ['Hydro pumped storage consumption', 'Cross border electricity trading','Load (incl. self-consumption)', 'Residual load', 'Renewable share of load']
+    unwanted_prod = ['Hydro pumped storage consumption', 'Cross border electricity trading',
+                     'Load (incl. self-consumption)', 'Residual load', 'Renewable share of load']
+    labeling = ["<10", "10-20", "20-30", "30-40","40-50","50-60","60-70",">70"]
+    bins = [0,10,20,30,40,50,60,70,100]
+    prod_types = ["Biomass","Coal", "Hydro",
+                  "Oil_Gas", "Others", "Solar",
+                  "Wind"]
 
     data = data_fetch(url, sql_cursor, "production", init)
 
     df = build_production_df(data, 'name', 'data', 'production_types', 'unix_seconds').drop(columns=unwanted_prod)
+    df = df.rename(columns={"Renewable share of generation":"Ren_share"})
 
     df["Total_Production"] = df.sum(axis=1)
 
     for key, values in prod_sums.items():
         df[key] = df[values].sum(axis=1)
 
+
     df = df.drop(columns=[item for sublist in prod_sums.values() for item in sublist if item != 'Others'])
+
+    df["Ren_share_bin"] = pd.cut(x=df["Ren_share"], bins=bins, right=False, labels=labeling)
+
+    # for ptype in prod_types:
+    #     df[ptype + "_pct"] = df[ptype].iloc[][ptype]
+
+    for ptype in prod_types:
+        df[f"{ptype}_pct"] = round(df[ptype] / df['Total_Production'], 2)
 
     pd.io.sql.to_sql(df, 'production', sql_connection, if_exists='append', index_label='Timestamp')
